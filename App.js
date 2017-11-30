eda57b9147c5a614224858aa544873418c7922e1
Nathaniel Sena <nate.sena1@gmail.com>
2017-11-29 16:28:28 -0800

    x's working in box

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {Button} from 'react-native-elements'
 
 export default class App extends React.Component {
+  state={
+    checkboxes:[[0,""], [1, ""], [2, ""], [3,""], [4, ""], [5, ""], [6, ""], [7, ""], [8, ""]]
+  }
+  hiya(index){
+   var originalBoxes = this.state.checkboxes
+   console.log('value of box is: ', originalBoxes[index][1])
+   if(originalBoxes[index][1] == ""){
+     console.log("that box was empty")
+     originalBoxes[index][1] = "x"
+    }
+   this.setState({
+     checkboxes: originalBoxes
+   }, ()=>{
+     console.log('just tried to set state')
+   })
+  }
   render() {
+    var squares = this.state.checkboxes
     return (
+    
       <View style={styles.container}>
+        {squares.map((square, index)=>{
+          return <Button key = {index} title={square[1]} onPress={()=>this.hiya(index)}/>
+        })}
+        
         <Text>What's Good Bro</Text>
       </View>
     );
